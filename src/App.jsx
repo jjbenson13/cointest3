7e17cf737c1e0c68379c2da7bcf81ff6df283b38
Jasmine Benson <jjortega2010@gmail.com>
2021-10-22 07:20:41 -0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import CoinDetailPage from './pages/CoinDetailPage'
 import CoinSummaryPage from './pages/CoinSummaryPage'
 import './App.css'
-import { WatchListContextProvider } from './context/WatchListContext'
 
 const App = () => {
   return (
     <div>
-      <WatchListContextProvider>
-        <BrowserRouter>
-          <Route exact path='/' component={CoinSummaryPage} />
-        </BrowserRouter>
-      </WatchListContextProvider>
+      <BrowserRouter>
+      <Route exact path='/' component={CoinSummaryPage}/>
+      </BrowserRouter>
     </div>
   )
-}
+} 
 
 export default App
